265945aad761e5d5ff3afb1e1e1c95846cb8b0f3
Auric Vente <mprops@protonmail.com>
2021-07-27 08:00:41 -0600

    First

---
+++ config.fish
@@ -0,0 +1,15 @@
+set fish_greeting
+
+source /home/yo/code/ezkl/ezkl.fish.sh
+
+alias get="sudo apt install -y"
+alias update="sudo apt update"
+alias upgrade="sudo apt -y dist-upgrade"
+alias up="update && upgrade"
+alias remove="sudo apt remove"
+alias autorm="sudo apt autoremove"
+alias h="history | grep"
+alias p="ps aux | grep"
+alias sauce="source ~/.bashrc"
+alias rm="rm -I"
+alias lq="lq -u"
