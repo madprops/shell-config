11ef107ba3fe050bdb418afecd8a380837756f2e
Auric Vente <mprops@protonmail.com>
2021-11-12 15:07:37 -0600

    Mod

--- config.fish
+++ config.fish
@@ -10,6 +10,8 @@ alias rm="rm -I"
 alias lq="/bin/lq -u"
 alias gitclone="git clone --depth 1"
 alias flatget="flatpak --installation=extra install"
+alias flatremove="flatpak --installation=extra uninstall"
+alias flatclean="flatpak --installation=extra uninstall --unused"
 alias flatrun="flatpak --installation=extra run"
 
 function search
